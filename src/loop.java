b65e263e193fe73c8128133c75559111febd1371
League Student <league@iMac-23.attlocal.net>
2020-01-23 16:58:55 -0800

    studying for test
    :100:
    :+1:

--- src/loop.java
+++ src/loop.java
@@ -17,5 +17,51 @@ public static void main(String[] args) {
 			System.out.println(i);
 		}
 	}
+	for(int i=0;i<555;i++) {
+		if(i%2==1) {
+			System.out.println(i+" is odd");
+		}else {
+			System.out.println(i+" is even");
+		}
+	}
+	for(int i=0;i<777;i++) {
+		if(i%7==0) {
+			System.out.println(i);
+		}
+	}
+	for(int i=0;i<12;i++) {
+		
+			System.out.println("in "+ (2007+ i) +" i was "+i+" years old");
+			
+	}
+	for(int i=0;i<3;i++) {
+		for(int j=0;j<3;j++) {
+				System.out.println(i+" "+j);
+	}		
+}
+	for(int i=0;i<3;i++) {
+		for(int j=0;j<3;j++) {
+			System.out.print(i*3+j+1);
+			
+			
+	}
+		System.out.println(" ");
+}	
+	for(int i=0;i<10;i++) {
+		for(int j=0;j<10;j++) {
+			System.out.print(i*10+j+1+" ");
+			
+			
+	}
+		System.out.println(" ");
+}
+	for(int i=0;i<7;i++) {
+		for(int j=0;j<i;j++) {
+			System.out.print("*");
+			
+			
+	}
+		System.out.println();
+}
 }
 }
