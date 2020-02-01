ee72f0a258cea4a2d900a30fd0cd441d9e76a745
League Student <league@iMac-13.attlocal.net>
2020-01-31 16:53:55 -0800

    still studying
    :scream:

--- src/_99_extra/EvilPolygonsChallenge.java
+++ src/_99_extra/EvilPolygonsChallenge.java
@@ -10,17 +10,38 @@ public class EvilPolygonsChallenge {
 	public static void main(String[] args) {
 		
 		//1. Create a new Robot
-
+Robot rob= new Robot();
 		//2. Set the speed to 100
-
-		int colorChoice=JOptionPane.showOptionDialog(null, "hello", "title", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Color 1","Color 2", "Color 3"}, 0);
+rob.setSpeed(100);
+		int colorChoice=JOptionPane.showOptionDialog(null, "hello", "title", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"red","green", "blue"}, 0);
 		
 		//3. Use if statements to check the the value of colorChoice and set the pen color accordingly 
-		
+		if(colorChoice==0) {
+			rob.setPenColor(1000,0,0);
+		}
+		if(colorChoice==1) {
+			rob.setPenColor(0,1000,0);
+		}
+		if(colorChoice==2) {
+			rob.setPenColor(0,0,1000);
+		}
 		//4. Ask the use how many polygons they want to be drawn.
-		
+		String polygons=JOptionPane.showInputDialog("how many polygons do you want?");
+		int polo=Integer.parseInt(polygons);
 		//5. Use the robot to draw the number of polygons the user requested.
-		
+		for(int i=polo;i>0;i--) {
+			rob.penDown();
+			rob.move(100);
+			rob.turn(110);
+			rob.move(50);
+			rob.turn(70);
+			rob.move(100);
+			rob.turn(110);
+			rob.move(50);
+			rob.turn(70);
+			rob.penUp();
+			rob.move(150);
+		}
 		//6. Make it so your shapes do not overlap
 
 		//7. Challenge: add more colors to the Option Dialog.
